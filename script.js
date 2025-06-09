b86853b664b822adff589bde057ba321fffd17a2
ShadowDevKat <211905275+ShadowDevKat@users.noreply.github.com>
2025-06-09 08:06:05 +0500

    Added event listeners to set grid size, clear grid
    consolidated grid methods, added method to set grid size and paint grid

--- script.js
+++ script.js
@@ -1,16 +1,59 @@
-let gridSize = 16;
+//Globals
+const defaultGridSize = 16;
+const minSize = 1;
+const maxSize = 100;
+let currentGridSize = defaultGridSize;
+const defaultBrushColor = "black";
+let currentBrushColor = defaultBrushColor;
 
 //Get references to html elements
 const gridContainer = document.getElementById("grid-container");
+const settingsContainer = document.querySelector(".settings-container");
+const currColorElem = document.getElementById("current-color");
+const currGridSizeElem = document.getElementById("grid-size");
 
-function createDiv(parent, cssClass) {
-  const div = document.createElement("div");
-  div.classList.add(cssClass);
-  parent.appendChild(div);
-  return div;
-}
+
+// Event listeners
+settingsContainer.addEventListener('click', (e) => {
+  let target = e.target;
+
+  switch (target.id) {
+    case 'rainbow-btn':
+      break;
+    case 'eraser-btn':
+      break;
+    case 'clear-btn':
+      init(currentGridSize, currentBrushColor);
+      break;
+    case 'grid-btn':
+      const newSize = prompt('Enter Grid Size (between 1 and 100)');
+      init(newSize);
+      break;
+    default:
+      break;
+  }
+});
+
+gridContainer.addEventListener('mousemove', (e) => {
+  let target = e.target;
+  if (target.classList.contains('cell')) {
+    paintCell(target);
+  }
+})
 
 function createGrid(container, size) {
+  function createDiv(parent, cssClass) {
+    const div = document.createElement("div");
+    div.classList.add(cssClass);
+    parent.appendChild(div);
+    return div;
+  }
+  function clearGrid(container) {
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+  }
+
   clearGrid(container);
 
   for (let x = 0; x < size; x++) {
@@ -21,10 +64,27 @@ function createGrid(container, size) {
   }
 }
 
-function clearGrid(container) {
-  while (container.firstChild) {
-    container.removeChild(container.firstChild);
-  }
+function showGridSize(textElem, size) {
+  textElem.textContent = `Grid: ${size} x ${size}`;
 }
 
-createGrid(gridContainer, gridSize);
+function paintCell(cell) {
+  if (cell.style.backgroundColor !== currentBrushColor) {
+    cell.style.backgroundColor = currentBrushColor;
+  }
+}
+function init(size = defaultGridSize, color = 'black') {
+  if (size < minSize || size > maxSize) {
+    currentGridSize = defaultGridSize;
+  }
+  else {
+    currentGridSize = size;
+  }
+  currentBrushColor = color;
+
+  paintCell(currColorElem, currentBrushColor);
+  showGridSize(currGridSizeElem, currentGridSize);
+  createGrid(gridContainer, currentGridSize);
+}
+
+init();
