5a77188d96c30999a039484bfa65edf13de1e86e
ShadowDevKat <211905275+ShadowDevKat@users.noreply.github.com>
2025-06-09 06:48:25 +0500

    Updated page layout and styling

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 let gridSize = 16;
 
 //Get references to html elements
-const gridContainer = document.getElementById('grid-container');
+const gridContainer = document.getElementById("grid-container");
 
 function createDiv(parent, cssClass) {
-    const div = document.createElement('div');
-    div.classList.add(cssClass);
-    parent.appendChild(div);
-    return div;
+  const div = document.createElement("div");
+  div.classList.add(cssClass);
+  parent.appendChild(div);
+  return div;
 }
 
 function createGrid(container, size) {
-    clearGrid(container);
+  clearGrid(container);
 
-    for (let x = 0; x < size; x++) {
-        const row = createDiv(container, 'row');
-        for (let y = 0; y < size; y++) {
-            createDiv(row, 'cell');
-        }
+  for (let x = 0; x < size; x++) {
+    const row = createDiv(container, "row");
+    for (let y = 0; y < size; y++) {
+      createDiv(row, "cell");
     }
+  }
 }
 
 function clearGrid(container) {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
 }
 
 createGrid(gridContainer, gridSize);
